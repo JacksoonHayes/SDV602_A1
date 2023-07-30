6dae4b6f42ab118927781257cd004f9e3ee88497
JacksoonHayes <jackson-hayes@live.nmit.ac.nz>
2023-07-31 10:57:48 +1200

    very basic story written of what the combat/inventory/status systems could look like

--- cmd_parser/command_manager.py
+++ cmd_parser/command_manager.py
@@ -1,9 +1,9 @@
 
 
 # Brief comment about how the following lines work
-game_state = 'Forest'
-game_places = {'Forest': {'Story': 'You are in the Forest\n\nTo the North is a Cave.\nTo the South is a Castle.\nTo the East is a Town\nTo the West is a Lake.',
-                          'North': 'Cave', 'South': 'Castle', 'East': 'Town', 'West': 'Lake', 'Image': 'forest.png'},
+game_state = 'Town'
+game_places = {'Town': {'Story': 'You are in a Town.\n\nTo the North is a Cave.\nTo the South is a Castle.\nTo the East is a Forest\nTo the West is a Lake.',
+                          'North': 'Cave', 'South': 'Castle', 'East': 'Forest', 'West': 'Lake', 'Image': 'town.png'},
                
                'Cave': {'Story': 'You are at the Cave.\n\nTo the South is Forest.\nTo the East is a Town.\nTo the West is a Lake.',
                         'North': '', 'South': 'Forest','East': 'Town', 'West': 'Lake', 'Image': 'cave.png'},
@@ -11,8 +11,8 @@ game_places = {'Forest': {'Story': 'You are in the Forest\n\nTo the North is a C
                'Castle': {'Story': 'You are at the Castle.\n\nTo the North is forest.\nTo the East is a Town.\nTo the West is a Lake.',
                           'North': 'Forest', 'South': '','East': 'Town', 'West': 'Lake', 'Image': 'castle.png'},
                
-               'Town': {'Story': 'You enter a Town.\n\nTo the North is a Cave.\nTo the West is a Forest.\nTo the South is a Castle.',
-                          'North': 'Cave', 'South': 'Castle', 'East': '', 'West': 'Forest',  'Image': 'town.png'},
+               'Forest': {'Story': 'You enter a Forest.\n\nTo the North is a Cave.\nTo the West is a Town.\nTo the South is a Castle.',
+                          'North': 'Cave', 'South': 'Castle', 'East': '', 'West': 'Town',  'Image': 'forest.png'},
                
                'Lake': {'Story': 'You arrive at a Lake.\n\nTo the North is a Cave.\nTo the East is a Forest.\nTo the South is a Castle.',
                           'North': 'Cave', 'South': 'Castle', 'East': 'Forest', 'West': '',  'Image': 'lake.png'},
@@ -46,7 +46,7 @@ def game_play(direction):
         game_place = game_places[game_state]
         proposed_state = game_place[direction.capitalize()]
         if proposed_state == '':
-            return 'You can not go that way.\n'+game_places[game_state]['Story']
+            return 'You can not go that way.\n\n'+game_places[game_state]['Story']
         else:
             game_state = proposed_state
             return game_places[game_state]['Story']
